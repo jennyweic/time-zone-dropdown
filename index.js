fa9573bd8c1b8989278051ee34a0163fb8b651c5
jennyweic <weicchan@gmail.com>
2023-11-17 15:05:16 +0000

    created each city timezone details & if structure

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function showCity(event) {
+  if (event.target.value === "paris") {
+    alert("");
+  }
 
+  if (event.target.value === "tokyo") {
+    alert("");
+  }
+
+  if (event.target.value === "sydney") {
+    alert("");
+  }
+}
+let selectedCity = document.querySelector("#city");
+selectedCity.addEventListener("change", showCity);
+
+//return paris current date and time
+let parisTimeDate = moment()
+  .tz("Europe/Paris")
+  .format("[It is ]dddd DD, MMMM YYYY, HH:mm:s A [in] ${tz}");
+let parisElement = document.querySelector("#paris");
+parisElement.innerHTML = parisTimeDate;
+
+//return tokyo current date and time
+let tokyoTimeDate = moment()
+  .tz("Asia/Tokyo")
+  .format("[It is ]dddd DD, MMMM YYYY, HH:mm:s A [in] ${tz}");
+let tokyoElement = document.querySelector("#tokyo");
+tokyoElement.innerHTML = tokyoTimeDate;
+
+//return sydney current date and time
+let sydneyTimeDate = moment()
+  .tz("Australia/Sydney")
+  .format("[It is ]dddd DD, MMMM YYYY, HH:mm:s A [in] ${tz}");
+let sydneyElement = document.querySelector("#sydney");
+sydneyElement.innerHTML = sydneyTimeDate;
